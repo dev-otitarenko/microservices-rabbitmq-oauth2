b3ed1e153ee0d37d0cc97981433c8277bb8fee4b
oleksii titarenko <atitarenko.eng@gmail.com>
2021-01-04 10:33:27 +0100

    events-service: init

---
+++ events-service/src/main/java/com/maestro/app/sample1/ms/events/aop/LogRequest.java
@@ -0,0 +1,12 @@
+package com.maestro.app.sample1.ms.events.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LogRequest {
+
+}
---
+++ events-service/src/main/java/com/maestro/app/sample1/ms/events/configuration/AopConfig.java
@@ -0,0 +1,10 @@
+package com.maestro.app.sample1.ms.events.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class AopConfig {
+
+}
